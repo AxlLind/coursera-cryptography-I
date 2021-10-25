fbca9f0ab1432e7b0dadb9817049d624bc62857c
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2021-10-25 08:46:27 +0200

    Add week 3 exercies

---
+++ src/week3.py
@@ -0,0 +1,26 @@
+import hashlib
+
+def chunks(s: bytes, n: int) -> list[bytes]:
+  return [bytes(s[i:i+n]) for i in range(0, len(s), n)]
+
+def sha256(*blocks: list[bytes]) -> bytes:
+  hasher = hashlib.sha256()
+  for block in blocks:
+    hasher.update(block)
+  return hasher.digest()
+
+def video_hash(path: str) -> str:
+  file = open(path, 'rb').read()
+  first_block, *blocks = reversed(chunks(file, 1024))
+  h = sha256(first_block)
+  for block in blocks:
+    h = sha256(block, h)
+  return h.hex()
+
+def main() -> None:
+  test_hash = "03c08f4ee0b576fe319338139c045c89c3e8e9409633bea29442e21425006ea8"
+  assert video_hash('6.2.birthday.mp4_download') == test_hash
+  print("Answer:", video_hash('6.1.intro.mp4_download'))
+
+if __name__ == "__main__":
+  main()
